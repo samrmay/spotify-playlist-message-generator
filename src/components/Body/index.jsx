5cb845dcd8c54f2be3b78a832881d4cd55eea75e
samrmay <samrmay@sas.upenn.edu>
2020-12-30 00:16:39 -0500

    added viewport meta tag

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -128,7 +128,7 @@ class Body extends React.Component {
                         userAccessToken={userAccessToken}
                         handleTrackRefresh={this.handleTrackRefresh}
                         handleReset={this.handleReset}/> 
-                    :   <div className={styles.instructions}>This is an app that creates playlists of songs that spell out whatever mesage you want. 
+                    :   <div className={styles.instructions}>This is an app that creates playlists of songs that spell out whatever message you want. 
                             Use it to check out new music, or save it as a brand new playlist for Spotify and share with friends, 
                             family, coworkers, bosses, enemies, whomever I guess.
                             <br />
